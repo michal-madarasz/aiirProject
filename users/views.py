0e617423a3aee1d28ddd36dc986839c39d57ec47
misiek <238903@student.pwr.edu.pl>
2019-05-17 11:41:48 +0200

    wybor liczby procesow

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.files.storage import default_storage
 from django.shortcuts import render, redirect
 
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, MpiParameters
 
 
 def register(request):
@@ -58,20 +58,25 @@ def file_upload(request):
 @login_required
 def task(request):
     if request.method == 'POST':
-        mpi = Popen(["mpirun", "-n", "3", "python3", "/home/misiek/3rok/mpi/Clustering_MPI.py"], stdout=PIPE)
+        m_form = MpiParameters(request.POST)
+        if m_form.is_valid():
+            n_process = m_form.cleaned_data['amount_of_process']
+            mpi = Popen(["mpirun", "-n", n_process, "python3", "/home/misiek/3rok/mpi/Clustering_MPI.py"], stdout=PIPE)
 
-        try:
-            outs, error = mpi.communicate(timeout=15)
-        except TimeoutExpired:
-            mpi.kill()
-            outs, error = mpi.communicate()
+            try:
+                outs, error = mpi.communicate(timeout=15)
+            except TimeoutExpired:
+                mpi.kill()
+                outs, error = mpi.communicate()
 
-        if outs != "":
-            print(str(outs, 'utf-8'))
-            messages.success(request, f'mpirun succesful')
-        else:
-            messages.error(request, f'mpi failure')
+            if outs != "":
+                print(str(outs, 'utf-8'))
+                messages.success(request, f'mpirun succesful')
+            else:
+                messages.error(request, f'mpi failure')
+            redirect('task')
     else:
+        m_form = MpiParameters()
         messages.error(request, f'No POST')
 
-    return render(request, 'users/task.html')
+    return render(request, 'users/task.html', {'m_form': m_form})
